51a99e50b6543eef022be0e8e0e4e60ccf6bb27c
Harry Jeffery <harry@exec64.co.uk>
2015-07-19 15:42:48 +0100

    Don't do clean builds on linux

--- build_linux.sh
+++ build_linux.sh
@@ -2,7 +2,7 @@
 
 pushd src
 ./devtools/bin/vpc /nh2 +everything /mksln everything &&
-make -j8 -f everything.mak clean &&
+#make -j8 -f everything.mak clean &&
 make -j8 -f everything.mak client_nh2 server_nh2
 popd
 cp game/nh2/bin/{server,client}.so .
