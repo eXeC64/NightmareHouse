14f67eb83f209c1f06ea35b1ec2a37220eac9f30
Harry Jeffery <harry@exec64.co.uk>
2015-07-27 21:25:23 +0100

    Don't force rebuild on windows

--- build_win.bat
+++ build_win.bat
@@ -1,7 +1,7 @@
 
 pushd src
 devtools\bin\vpc /2010 /nh2 +game /mksln nightmarehouse.sln
-devenv nightmarehouse.sln /Rebuild Release
+devenv nightmarehouse.sln /Build Release
 popd
 copy game\nh2\bin\client.dll client.dll
 copy game\nh2\bin\server.dll server.dll
