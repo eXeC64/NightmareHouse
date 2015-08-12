06a658f005eb4a994ffe2c229eb74dcf22cba8e1
Harry Jeffery <harry@exec64.co.uk>
2015-08-12 19:43:13 +0100

    Animate sprintbar

--- src/game/client/nh2/hud_sprintbar.cpp
+++ src/game/client/nh2/hud_sprintbar.cpp
@@ -24,10 +24,12 @@ public:
 
 	virtual void Init (void);
 	virtual void Reset (void);
+	virtual void OnThink (void);
 
 protected:
 	virtual void Paint();
 
+	float m_flPower;
 	vgui::IImage* m_pBar;
 	vgui::IImage* m_pBackground;
 	CPanelAnimationVar(vgui::HFont, m_hIconFont, "IconFont", "NH2HUDIcons");
@@ -64,20 +66,29 @@ void CHudSprintBar::Reset (void)
 {
 	SetFgColor(Color(255,255,255,255));
 	SetBgColor(Color(0,0,0,0));
+	m_flPower = 100;
+	SetAlpha(0);
 }
 
-void CHudSprintBar::Paint()
+void CHudSprintBar::OnThink()
 {
 	C_BaseHLPlayer *pPlayer = (C_BaseHLPlayer *)C_BasePlayer::GetLocalPlayer();
 	if ( !pPlayer )
 		return;
 
-	const float flPower = pPlayer->m_HL2Local.m_flSuitPower;
+	const float power = pPlayer->m_HL2Local.m_flSuitPower;
+	if (m_flPower != power)
+	{
+		if (power >= 100 && m_flPower < 100)
+			g_pClientMode->GetViewportAnimationController()->RunAnimationCommand( this, "alpha", 0.0f, 0.0f, 0.4f, vgui::AnimationController::INTERPOLATOR_LINEAR);
+		else if(power < 100 && m_flPower >= 100)
+			g_pClientMode->GetViewportAnimationController()->RunAnimationCommand( this, "alpha", 255.0f, 0.0f, 0.4f, vgui::AnimationController::INTERPOLATOR_LINEAR);
+		m_flPower = power;
+	}
+}
 
-  // Don't draw a full bar
-	if (flPower >= 100.0)
-	  return;
-
+void CHudSprintBar::Paint()
+{
 	int panelWidth, panelHeight;
 	GetSize(panelWidth, panelHeight);
 
@@ -88,12 +99,12 @@ void CHudSprintBar::Paint()
 
 	m_pBar->SetSize(panelWidth*m_flBarWidth, panelHeight*m_flBarHeight);
 	m_pBar->SetPos(panelWidth*m_flBarInsetX, panelHeight*m_flBarInsetY);
-	m_pBar->SetFrame(max(0,flPower));
+	m_pBar->SetFrame(min(max(0,m_flPower),99));
 	m_pBar->Paint();
 
 	//Draw sprint icon
 	vgui::surface()->DrawSetTextFont(m_hIconFont);
-	vgui::surface()->DrawSetTextColor(Color(255,flPower > 21 ? 255 : 0, flPower > 21 ? 255 : 0, m_iIconAlpha));
+	vgui::surface()->DrawSetTextColor(Color(255,m_flPower > 21 ? 255 : 0, m_flPower > 21 ? 255 : 0, m_iIconAlpha));
 	vgui::surface()->DrawSetTextPos(panelWidth*m_flIconX, panelHeight*m_flIconY);
 	vgui::surface()->DrawPrintText(L"D",1);
 }
