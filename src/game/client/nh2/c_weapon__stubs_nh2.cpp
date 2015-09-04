03c389c2c70f7ddeffa383a3b6a7b922639b8d21
Harry Jeffery <harry@exec64.co.uk>
2015-09-04 21:24:48 +0100

    add weapon_nh_hatchet

---
+++ src/game/client/nh2/c_weapon__stubs_nh2.cpp
@@ -0,0 +1,15 @@
+//========= Copyright Valve Corporation, All rights reserved. ============//
+//
+// Purpose: 
+//
+//=============================================================================//
+
+#include "cbase.h"
+#include "c_weapon__stubs.h"
+#include "basehlcombatweapon_shared.h"
+#include "c_basehlcombatweapon.h"
+
+// memdbgon must be the last include file in a .cpp file!!!
+#include "tier0/memdbgon.h"
+
+STUB_WEAPON_CLASS( weapon_nh_hatchet, WeaponNHHatchet, C_BaseHLBludgeonWeapon );
---
+++ src/game/server/nh2/weapon_nh_hatchet.h
@@ -0,0 +1,50 @@
+//========= Copyright Valve Corporation, All rights reserved. ============//
+//
+// Purpose:
+//
+//=============================================================================//
+
+#ifndef WEAPON_NH_HATCHET_H
+#define WEAPON_NH_HATCHET_H
+
+#include "basebludgeonweapon.h"
+
+#if defined( _WIN32 )
+#pragma once
+#endif
+
+#define	HATCHET_RANGE	75.0f
+#define	HATCHET_REFIRE	0.4f
+
+//-----------------------------------------------------------------------------
+// CWeaponNHHatchet
+//-----------------------------------------------------------------------------
+
+class CWeaponNHHatchet : public CBaseHLBludgeonWeapon
+{
+public:
+	DECLARE_CLASS( CWeaponNHHatchet, CBaseHLBludgeonWeapon );
+
+	DECLARE_SERVERCLASS();
+	DECLARE_ACTTABLE();
+
+	CWeaponNHHatchet();
+
+	float		GetRange( void )		{	return	HATCHET_RANGE;	}
+	float		GetFireRate( void )		{	return	HATCHET_REFIRE;	}
+
+	void		AddViewKick( void );
+	float		GetDamageForActivity( Activity hitActivity );
+
+	virtual int WeaponMeleeAttack1Condition( float flDot, float flDist );
+	void		SecondaryAttack( void )	{	return;	}
+
+	// Animation event
+	virtual void Operator_HandleAnimEvent( animevent_t *pEvent, CBaseCombatCharacter *pOperator );
+
+private:
+	// Animation event handlers
+	void HandleAnimEventMeleeHit( animevent_t *pEvent, CBaseCombatCharacter *pOperator );
+};
+
+#endif // WEAPON_NH_HATCHET_H
