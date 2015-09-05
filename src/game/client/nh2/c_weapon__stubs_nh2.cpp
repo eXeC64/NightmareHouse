fa62c1e771dc7238cf294b08e0a58f26081a795d
Harry Jeffery <harry@exec64.co.uk>
2015-09-05 02:01:16 +0100

    add nh_weapon_revolver

--- src/game/client/nh2/c_weapon__stubs_nh2.cpp
+++ src/game/client/nh2/c_weapon__stubs_nh2.cpp
@@ -12,5 +12,6 @@
 // memdbgon must be the last include file in a .cpp file!!!
 #include "tier0/memdbgon.h"
 
-STUB_WEAPON_CLASS( weapon_nh_pistol, WeaponNHPistol, C_BaseHLCombatWeapon );
 STUB_WEAPON_CLASS( weapon_nh_hatchet, WeaponNHHatchet, C_BaseHLBludgeonWeapon );
+STUB_WEAPON_CLASS( weapon_nh_pistol, WeaponNHPistol, C_BaseHLCombatWeapon );
+STUB_WEAPON_CLASS( weapon_nh_revolver, WeaponNHRevolver, C_BaseHLCombatWeapon );
