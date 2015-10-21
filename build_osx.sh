7afd026b1aa067f9ee392e772081986963a87da7
Harry Jeffery <harry@exec64.co.uk>
2015-10-21 18:52:22 +0100

    Fix OSX compiling

--- build_osx.sh
+++ build_osx.sh
@@ -2,6 +2,6 @@
 
 pushd src
 ./devtools/bin/vpc /nh2 +everything /mksln everything &&
-xcodebuild -project sp/src/everything.xcodeproj -configuration Release -target "client (nh2)" -target "server (nh2)"
+xcodebuild -project everything.xcodeproj -configuration Release -target "client (nh2)" -target "server (nh2)"
 popd
 cp game/nh2/bin/{client,server}.dylib .
